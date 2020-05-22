ccfd8017e04630e050911d131afcf48dde5445c8
Nadeaun <60448113+Nadeaun@users.noreply.github.com>
2020-05-22 12:59:42 -0700

    Finished Main Menus Scene
    Basic three button menus Play, Controls and Quit all functional

---
+++ Out of Time Capstone/Assets/Scripts/MainMenus.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenus : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main_Cabin");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
