f177a1b43e17a4cd6440534c2057ba8d24c3785b
taylav5991 <46456195+taylav5991@users.noreply.github.com>
2020-05-23 14:17:41 -0700

    can pick up Pendelum using E key
    PRESS E TO PICK UP AND G TO PUT DOWN ( bug pendellum goes through walls still fixing)

---
+++ Out of Time Capstone/Assets/Scripts/PickUp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+
+    public Transform theDestination;
+   
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            GetComponent<BoxCollider>().enabled = false;
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().freezeRotation = false;
+            this.transform.position = theDestination.position;
+            this.transform.parent = GameObject.Find("Destination").transform;
+
+
+        }
+
+        if(Input.GetKeyUp(KeyCode.G))
+        {
+            this.transform.parent = null;
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<BoxCollider>().enabled = true;
+
+        }
+
+
+    }
+}
