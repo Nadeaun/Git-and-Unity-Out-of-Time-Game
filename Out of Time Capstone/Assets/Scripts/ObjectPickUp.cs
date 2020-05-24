3396c5948ae4798d37893f8d7f17aa785e22b919
taylav5991 <46456195+taylav5991@users.noreply.github.com>
2020-05-24 16:06:59 -0700

    Bugs wit Picking up Pendellum
    Tried to fix bugs but was unsuccessful

--- Out of Time Capstone/Assets/Scripts/ObjectPickUp.cs
+++ Out of Time Capstone/Assets/Scripts/ObjectPickUp.cs
@@ -4,30 +4,56 @@ using UnityEngine;
 
 public class ObjectPickUp : MonoBehaviour
 {
-    public Transform inHand;
+    public bool canHold = true;
+    Vector3 objectPosition;
+    public GameObject theItem;
+    public GameObject Parent;
+    public bool isHolding = false;
+    float distance;
 
+    void Update()
+    {
+        distance = Vector3.Distance(theItem.transform.position, Parent.transform.position);
+        if(distance >=1f)
+        {
+            isHolding = false;
+        }
+            
+        if(isHolding==true)
+        {
+            theItem.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            theItem.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            theItem.transform.SetParent(Parent.transform);
+            
+        }
+        else
+        {
+            objectPosition = theItem.transform.position;
+            theItem.transform.SetParent(null);
+            theItem.GetComponent<Rigidbody>().useGravity = true;
+            theItem.transform.position = objectPosition;
+        }
+    }
     void FixedUpdate()
     {
         if(Input.GetKeyDown(KeyCode.E))
         {
-            GetComponent<Rigidbody>().useGravity = false;
-            this.transform.position = inHand.transform.position;
-            this.transform.parent = GameObject.Find("First Person Player").transform;
-            this.transform.parent = GameObject.Find("Main Camera").transform;
+            if(distance<=1f)
+            {
+                isHolding = true;
+                theItem.GetComponent<Rigidbody>().useGravity = false;
+
+                theItem.GetComponent<Rigidbody>().detectCollisions = true;
+            }
+           
+
         }
 
-        if (Input.GetKeyUp(KeyCode.E))
+        if(Input.GetKeyUp(KeyCode.G))
         {
-            this.transform.parent = null;
-            GetComponent<Rigidbody>().useGravity = true;
+            isHolding = false;
         }
-
 
 
     }
-
-
-
-   
 }
-
--- Out of Time Capstone/Assets/Scripts/PickUp.cs
+++ Out of Time Capstone/Assets/Scripts/PickUp.cs
@@ -5,36 +5,48 @@ using UnityEngine;
 public class PickUp : MonoBehaviour
 {
    
-    public Transform theDestination;
-  
    
+    public Transform theDestination;
+    public GameObject theItem;
+
+
     // Update is called once per frame
     void FixedUpdate()
     {
-      
-        if(Input.GetKeyDown(KeyCode.E))
+
+
+
+        if (Input.GetKeyDown(KeyCode.E))
         {
+
             GetComponent<BoxCollider>().enabled = false;
             GetComponent<Rigidbody>().useGravity = false;
             GetComponent<Rigidbody>().freezeRotation = false;
             GetComponent<Rigidbody>().detectCollisions = true;
-        
+
+           
             this.transform.position = theDestination.position;
             this.transform.parent = GameObject.Find("Destination").transform;
 
-
+            //GetKeyDown
         }
-
-        if(Input.GetKeyUp(KeyCode.E))
+        if (Input.GetKeyUp(KeyCode.E))
         {
             this.transform.parent = null;
             GetComponent<Rigidbody>().useGravity = true;
             GetComponent<Rigidbody>().detectCollisions = true;
             GetComponent<BoxCollider>().enabled = true;
-            
 
         }
 
+        //GetKeyUp
+
+
 
     }
+
+    
+        
+    
+        
 }
