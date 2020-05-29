e6e43999b578929af13753a4cb5d79fb36b97aef
taylav5991 <46456195+taylav5991@users.noreply.github.com>
2020-05-29 12:10:07 -0700

    test

---
+++ Out of Time Capstone/Assets/Scripts/ItemPickUp.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    public Transform player;
+    public Transform Camera;
+    bool inItemRange = false;
+    bool carryItem=false;
+    private bool collideWithWall=false;
+
+
+    void Update()
+    {
+        float distance = Vector3.Distance(gameObject.transform.position, player.position);
+        if(distance <=2.5f)
+        {
+            inItemRange = true;
+        }
+        else
+        {
+            inItemRange = false;
+        }
+        if(inItemRange && Input.GetKeyUp(KeyCode.E))
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            transform.parent = Camera;
+            carryItem = true;
+        }
+
+        if(carryItem)
+        {
+            if(collideWithWall)
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                carryItem = false;
+                collideWithWall = false;
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.E))
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+            transform.parent = null;
+            carryItem = false;
+        }
+        
+     
+    }
+
+
+
+    void OnTriggerEnter()
+    {
+        if (carryItem)
+        {
+            collideWithWall = true;
+        }
+    }
+
+
+
+
+
+
+}
--- Out of Time Capstone/Assets/Scripts/PickUp.cs
+++ Out of Time Capstone/Assets/Scripts/PickUp.cs
@@ -4,49 +4,72 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-   
-   
+
+
     public Transform theDestination;
-    public GameObject theItem;
+    //  public GameObject theItem;
+    public bool canHold;
+    // no objects are  currently  being held so can hold object
+    public bool cannotHold;// cant hold any other objects when one object is already being held
 
-
-    // Update is called once per frame
-    void FixedUpdate()
+    void OnTriggerEnter(Collider col)
     {
-
-
-
-        if (Input.GetKeyDown(KeyCode.E))
+        if (col.gameObject.tag == "Pendulum")
         {
-
-            GetComponent<BoxCollider>().enabled = false;
-            GetComponent<Rigidbody>().useGravity = false;
-            GetComponent<Rigidbody>().freezeRotation = false;
-            GetComponent<Rigidbody>().detectCollisions = true;
-
-           
-            this.transform.position = theDestination.position;
-            this.transform.parent = GameObject.Find("Destination").transform;
-
-            //GetKeyDown
+            canHold = true;
         }
-        if (Input.GetKeyUp(KeyCode.E))
+        else if (col.gameObject.tag == "Wind Up Key" && canHold)
         {
-            this.transform.parent = null;
-            GetComponent<Rigidbody>().useGravity = true;
-            GetComponent<Rigidbody>().detectCollisions = true;
-            GetComponent<BoxCollider>().enabled = true;
+            cannotHold = true;
 
         }
-
-        //GetKeyUp
 
 
 
     }
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+
+
+            //this is a function to PICK UP OBJECT when E KEY is pressed (gravity is false)
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+
+                GetComponent<BoxCollider>().enabled = false;
+                GetComponent<Rigidbody>().useGravity = false;
+
+                GetComponent<Rigidbody>().freezeRotation = false;
+                GetComponent<Rigidbody>().detectCollisions = true;
+
+
+                this.transform.position = theDestination.position;
+                this.transform.parent = GameObject.Find("Destination").transform;
+
+                //GetKeyDown
+            }
+
+            // this is a function when OBJECT IS ALREADY BEING PICKED UP you press E KEY to drop ( gravity is set back to true)
+
+            else if (Input.GetKeyUp(KeyCode.E))
+            {
+
+                this.transform.parent = null;
+                GetComponent<Rigidbody>().useGravity = true;
+                GetComponent<Rigidbody>().detectCollisions = true;
+                GetComponent<BoxCollider>().enabled = true;
+
+            }
+
+            //GetKeyUp
+
+
+
+        }
+
+
+
+
 
     
-        
-    
-        
 }
