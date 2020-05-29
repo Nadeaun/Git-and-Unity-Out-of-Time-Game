828e124e50ac9f0327bcdc1b76bb850027e5b131
Nadeaun <60448113+Nadeaun@users.noreply.github.com>
2020-05-29 12:55:52 -0700

    Re-Fixed the ItemPickUp script

--- Out of Time Capstone/Assets/Scripts/ItemPickUp.cs
+++ Out of Time Capstone/Assets/Scripts/ItemPickUp.cs
@@ -2,88 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
-public class ItemPickUp : MonoBehaviour
-{
-    public Transform player;
-    public Transform Camera;
-    bool inItemRange = false;
-    bool carryItem=false;
-    private bool collideWithWall=false;
-
-
-    void Update()
-    {
-        float distance = Vector3.Distance(gameObject.transform.position, player.position);
-        if(distance <=2.5f)
-        {
-            inItemRange = true;
-        }
-        else
-        {
-            inItemRange = false;
-        }
-        if(inItemRange && Input.GetKeyUp(KeyCode.E))
-        {
-            GetComponent<Rigidbody>().isKinematic = true;
-            transform.parent = Camera;
-            carryItem = true;
-        }
-
-        if(carryItem)
-        {
-            if(collideWithWall)
-            {
-                GetComponent<Rigidbody>().isKinematic = false;
-                transform.parent = null;
-                carryItem = false;
-                collideWithWall = false;
-            }
-        }
-        else if(Input.GetKeyDown(KeyCode.E))
-        {
-            GetComponent<Rigidbody>().isKinematic = false;
-            transform.parent = null;
-            carryItem = false;
-        }
-        
-     
-    }
-
-
-
-    void OnTriggerEnter()
-    {
-        if (carryItem)
-        {
-            collideWithWall = true;
-        }
-    }
-
-
-
-
-
-
-}
-=======
 
 public class ItemPickUp : MonoBehaviour
 {
-    
+
     public Transform hand;
     GameObject held_item;
     public bool isHolding = false;
     public bool lighterActive;
 
     // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         // Check if player is already holding something in their hand (besides lighter)
         if (isHolding == false)
         {
-            lighterActive = false;
-
+            
             if (Input.GetMouseButtonDown(0))
             {
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -97,22 +31,14 @@ public class ItemPickUp : MonoBehaviour
                         item.GetComponent<Rigidbody>().isKinematic = true;
                         item.transform.SetPositionAndRotation(hand.transform.position, hand.rotation);
                         item.transform.parent = hand.transform;
-                        isHolding = true;
                         
+                        // Set to true so that you can't pick up another item
+                        isHolding = true;
+                        // Make you put away your lighter
+                        lighterActive = false;
                     }
-                    
-                }
-                else
-                {
-                    Debug.Log("No hit :(");
                 }
             }
         }
     }
-
-    void dropItem()
-    {
-        held_item.GetComponent<Rigidbody>().isKinematic = false;
-    }
 }
->>>>>>> 4b58b593e0eca18ee16e37edcacad6f1be00e0de
