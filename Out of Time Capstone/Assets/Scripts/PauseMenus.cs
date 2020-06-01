ae76f6fe341de8b024bb610bec5746a0605531d4
Nadeaun <60448113+Nadeaun@users.noreply.github.com>
2020-05-31 17:54:59 -0700

    Fixed Pause menu so it will quit game
    NOTE: For some reason the hover-animation for the quit button in the pause menu is not playing

---
+++ Out of Time Capstone/Assets/Scripts/PauseMenus.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenus : MonoBehaviour
+{
+
+    public bool GameIsPaused = false;
+    public GameObject pauseMenusUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenusUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Pause()
+    {
+        pauseMenusUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
