9ea0332b79809bcbcf53fcc8c5a910dd5bef1a0f
taylav5991 <46456195+taylav5991@users.noreply.github.com>
2020-06-07 20:24:54 -0700

    Tried Pick Up display text but doesn't work quite yet

---
+++ Out of Time Capstone/Assets/Scripts/DisplayPickUpText.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayPickUpText : MonoBehaviour
+{
+    public float theDistance;
+
+    public GameObject pickUpItemText;
+  
+    // Update is called once per frame
+    void Update()
+    {
+        theDistance = RaycastItem.DistanceFromTarget;
+    }
+
+
+    void OnMouseOver()
+    {
+
+        if (theDistance <= 5)
+        {
+            pickUpItemText.SetActive(true);
+
+        }
+
+
+    }
+
+    void OnMouseExit()
+    {
+        pickUpItemText.SetActive(false);
+    
+    }
+
+
+}
